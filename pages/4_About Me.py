71c987710ef747e72a14da8e64e7b1295e8fee31
Mohamed Ahmed Mohamed ElSayed <ma43892992009@gmail.com>
2025-11-01 17:31:55 +0300

    Update 4_About Me.py

--- pages/4_About Me.py
+++ pages/4_About Me.py
@@ -11,8 +11,9 @@ def main():
     st.write("University: AlShorouk Academy")
     st.write("Email:", "[ma4385019@gmail.com](mailto:ma4385019@gmail.com)")
     st.write("GitHub:", "[https://github.com/MohamedAhmed3520](https://github.com/MohamedAhmed3520)")
-    st.write("LinkedIn:", "[https://www.linkedin.com/in/mohamed-ahmed-000b5929a/](https://www.linkedin.com/in/mohamed-ahmed-000b5929a/)")
+    st.write("LinkedIn:", "[https://www.linkedin.com/in/mohamed-ahmed-372730373/](https://www.linkedin.com/in/mohamed-ahmed-372730373/)")
     st.write("Kaggle:", "[https://www.kaggle.com/mohamedahmedlsayed](https://www.kaggle.com/mohamedahmedlsayed)")
     
 if __name__ == "__main__":
     main()
+
